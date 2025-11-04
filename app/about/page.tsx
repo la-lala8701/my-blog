bf5312db6ef88efc1984aa9c4c43e32559f687ee
Saki Nakata <nakata@dotinstall.com>
2025-11-04 17:03:21 +0900

    💄 記事ページのスタイル調整

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,26 +1,34 @@
+import { Container } from "../components/Container";
 import { Header } from "../components/Header";
 
 export default function About() {
   return (
     <div>
       <Header />
-      <h1>タイトル</h1>
-      {/* <img src="/broken-image.jpg" /> */}
-      <p>userName</p>
-      <p>2025年04月08日</p>
-      {/* <img src="" alt="heart" /> */}
-      <p>
-        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur unde suscipit, quam beatae rerum inventore
-        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-        fugiat deleniti? Eum quasi quidem quibusdam.
-      </p>
-      <p>
-        body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
-        blanditiis tenetur unde suscipit, quam beatae rerum inventore
-        consectetur, neque doloribus, cupiditate numquam dignissimos laborum
-        fugiat deleniti? Eum quasi quidem quibusdam.
-      </p>
+      <Container>
+        <h1 className="text-5xl font-bold mb-8">タイトルが入ります</h1>
+        <div className="flex items-start gap-2">
+          <div className="w-7 h-7 bg-gray-400 rounded-full"></div>
+          <div>
+            <p className="text-base">UserName</p>
+            <p className="text-sm text-gray-500">2025年04月08日</p>
+          </div>
+        </div>
+        <div className="mt-16 space-y-4">
+          <p>
+            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+            blanditiis tenetur unde suscipit, quam beatae rerum inventore
+            consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+            fugiat deleniti? Eum quasi quidem quibusdam.
+          </p>
+          <p>
+            body1. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos
+            blanditiis tenetur unde suscipit, quam beatae rerum inventore
+            consectetur, neque doloribus, cupiditate numquam dignissimos laborum
+            fugiat deleniti? Eum quasi quidem quibusdam.
+          </p>
+        </div>
+      </Container>
     </div>
   );
 }
