974f3b31763d5ddf995196d5628dba4d112c7c84
Saki Nakata <nakata@dotinstall.com>
2025-11-04 16:36:44 +0900

    ✨ ヘッダーコンポーネントのスタイルを追加し、レイアウトを改善

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,14 +2,19 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className="p-6 border-b border-gray-300">
+      <div className="flex items-center justify-between">
+        <h1 className="p-1.5 text-2xl font-bold">
+          <Link href="/">Home</Link>
+        </h1>
+        <nav>
+          <ul>
+            <li className="p-1.5">
+              <Link href="/about">記事作成</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
