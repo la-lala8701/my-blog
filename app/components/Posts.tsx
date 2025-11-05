3442def846739d9f947038bdb873e8dac50c5d42
Saki Nakata <nakata@dotinstall.com>
2025-11-05 11:47:14 +0900

    ✨ Postsコンポーネントを修正し、postDataを使用して記事一覧を動的に表示

--- app/components/Posts.tsx
+++ app/components/Posts.tsx
@@ -1,16 +1,15 @@
-import Link from "next/link";
+import { postData } from "../utils/placeholder-data";
 import { PostsItem } from "./PostsItem";
 
 export const Posts = () => {
   return (
-    <div>
+    <section>
       <h2 className="text-3xl pb-6">記事一覧</h2>
       <div className="grid grid-cols-1 gap-4">
-        <PostsItem />
-        <PostsItem />
-        <PostsItem />
-        <PostsItem />
+        {postData.map((post) => (
+          <PostsItem key={post.id} {...post} />
+        ))}
       </div>
-    </div>
+    </section>
   );
 };
