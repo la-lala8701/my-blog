9040c410723fa918204c2878b7ef71eec1f73abf
Saki Nakata <nakata@dotinstall.com>
2025-11-05 15:27:53 +0900

    🩹 スタイルの記述箇所変更

--- app/components/PostsItem.tsx
+++ app/components/PostsItem.tsx
@@ -3,8 +3,8 @@ import { PostData } from "../utils/placeholder-data";
 
 export const PostsItem = (props: PostData) => {
   return (
-    <article className="border-2 border-gray-300 rounded-lg p-4 hover:bg-gray-100">
-        <Link href={`/post/${props.id}`} className="block">
+    <article>
+        <Link  href={`/post/${props.id}`} className="block border-2 border-gray-300 rounded-lg p-4 hover:bg-gray-100">
           <h3 className="text-xl font-bold mb-3">{props.title}</h3>
           <div className="flex items-start gap-2">
             <div className="w-7 h-7 bg-gray-400 rounded-full"></div>
