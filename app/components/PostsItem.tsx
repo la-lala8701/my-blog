c0cffaa737ff33a5ce7a8ca1cf1bb0404b680a1b
Saki Nakata <nakata@dotinstall.com>
2025-11-05 11:47:26 +0900

    ✨ PostsItemコンポーネントを修正し、propsを使用して記事情報を動的に表示

--- app/components/PostsItem.tsx
+++ app/components/PostsItem.tsx
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import { PostData } from "../utils/placeholder-data";
 
-export const PostsItem = () => {
+export const PostsItem = (props: PostData) => {
   return (
     <article className="border-2 border-gray-300 rounded-lg p-4 hover:bg-gray-100">
-        <Link href="/about">
-          <h3 className="text-xl font-bold mb-3">タイトルが入ります</h3>
+        <Link href={`/post/${props.id}`} className="block">
+          <h3 className="text-xl font-bold mb-3">{props.title}</h3>
           <div className="flex items-start gap-2">
             <div className="w-7 h-7 bg-gray-400 rounded-full"></div>
             <div>
-              <p className="text-base">UserName</p>
-              <p className="text-sm text-gray-500">2025年04月08日</p>
+              <p className="text-base">{props.author}</p>
+              <p className="text-sm text-gray-500">{props.date}</p>
             </div>
           </div>
         </Link>
