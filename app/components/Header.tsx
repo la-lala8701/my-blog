7ec9099bedb881d1c4b589757851f0aae1c53d06
Saki Nakata <nakata@dotinstall.com>
2025-11-05 15:27:00 +0900

    🩹 スタイルの修正と、ボタンの追加

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -2,15 +2,30 @@ import Link from "next/link";
 
 export const Header = () => {
   return (
-    <header className="p-6 border-b border-gray-300">
+    <header className="py-6 px-14 border-b border-gray-300">
       <div className="flex items-center justify-between">
-        <h1 className="p-1.5 text-2xl font-bold">
-          <Link href="/">Home</Link>
+        <h1 className="text-2xl font-bold">
+          <Link className="p-1.5" href="/">
+            Home
+          </Link>
         </h1>
         <nav>
-          <ul>
-            <li className="p-1.5">
-              <Link href="/about">記事作成</Link>
+          <ul className="flex">
+            <li>
+              <Link
+                className="ml-4 px-4 py-3 bg-green-300 rounded-md hover:bg-green-400"
+                href="/create"
+              >
+                記事作成
+              </Link>
+            </li>
+            <li>
+              <Link
+                className="px-4 py-3 bg-blue-300 rounded-md hover:bg-blue-400"
+                href="/login"
+              >
+                ログイン
+              </Link>
             </li>
           </ul>
         </nav>
