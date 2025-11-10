c9ab9e632f0b2e389cde6b13472d2b9233a2b3e2
Saki Nakata <nakata@dotinstall.com>
2025-11-10 15:51:32 +0900

    ✨ 削除ボタン追加

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -18,6 +18,7 @@ export default function Post() {
             記事を編集
           </button>
         </Link>
+        <button className="ml-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 cursor-pointer">削除</button>
       </Container>
     </div>
   );
