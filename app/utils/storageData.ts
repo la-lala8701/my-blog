3c386e076a2ab61e1e356db6ff07815c1b1e0666
Saki Nakata <nakata@dotinstall.com>
2025-11-10 12:15:53 +0900

    🏷️型定義を厳格化しました

--- app/utils/storageData.ts
+++ app/utils/storageData.ts
@@ -1,6 +1,6 @@
 import { PostData } from "../types";
 
-export const storageData = (key: string): PostData[] => {
+export const storageData = (key: "posts"): PostData[] => {
   if (typeof window !== "undefined") {
     const storedPosts = localStorage.getItem(key);
     return storedPosts ? JSON.parse(storedPosts) : [];
