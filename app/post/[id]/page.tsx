97191a5c7a9738ba6d80674b8afad706ff0565a3
Saki Nakata <nakata@dotinstall.com>
2025-11-10 16:39:33 +0900

    ✨ 削除機能を追加しました

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -4,10 +4,17 @@ import { Container } from "../../components/Container";
 import { Header } from "../../components/Header";
 import { Article } from "@/app/components/Article";
 import { useParams } from "next/navigation";
-
+import { storageData } from "@/app/utils/storageData";
+import { useCallback } from "react";
 
 export default function Post() {
   const params = useParams();
+  const posts = storageData("posts");
+
+  const handleDelete = useCallback(() => {
+    const updatedPosts = posts.filter((post) => post.id !== params.id);
+    localStorage.setItem("posts", JSON.stringify(updatedPosts));
+  }, []);
   return (
     <div>
       <Header />
@@ -18,7 +25,14 @@ export default function Post() {
             記事を編集
           </button>
         </Link>
-        <button className="ml-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 cursor-pointer">削除</button>
+        <Link href="/">
+          <button
+            onClick={handleDelete}
+            className="ml-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 cursor-pointer"
+          >
+            削除
+          </button>
+        </Link>
       </Container>
     </div>
   );
