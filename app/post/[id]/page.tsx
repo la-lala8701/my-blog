088635b8ab47fef7558cafac077eec2f31436e7d
Saki Nakata <nakata@dotinstall.com>
2025-11-10 13:54:08 +0900

    ♻️ useParamsを使用して記事編集リンクを動的に更新

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,18 +1,20 @@
+"use client";
 import Link from "next/link";
 import { Container } from "../../components/Container";
 import { Header } from "../../components/Header";
 import { Article } from "@/app/components/Article";
+import { useParams } from "next/navigation";
 
 
 export default function Post() {
-
+  const params = useParams();
   return (
     <div>
       <Header />
       <Container>
         <Article />
-        <Link href={`/post/${"id"}/edit`}>
-          <button className="mt-8 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600">
+        <Link href={`/post/${params.id}/edit`}>
+          <button className="mt-8 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 cursor-pointer">
             記事を編集
           </button>
         </Link>
