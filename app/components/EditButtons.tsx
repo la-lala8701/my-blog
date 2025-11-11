703633a3fadefff851d882d6d61fa4a618b17dc1
Saki Nakata <nakata@dotinstall.com>
2025-11-11 16:34:02 +0900

    ♻️ 編集、削除ボタン部分のコンポーネント化

---
+++ app/components/EditButtons.tsx
@@ -0,0 +1,33 @@
+"use client";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import { storageData } from "../utils/storageData";
+import { useCallback } from "react";
+
+export const EditButtons = () => {
+  const params = useParams<{ id: string }>();
+  const posts = storageData("posts");
+
+  const handleDelete = useCallback(() => {
+    const updatedPosts = posts.filter((post) => post.id !== params.id);
+    localStorage.setItem("posts", JSON.stringify(updatedPosts));
+  }, [params.id, posts]);
+
+  return (
+    <div>
+      <Link href={`/post/${params.id}/edit`}>
+        <button className="mt-8 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 cursor-pointer">
+          記事を編集
+        </button>
+      </Link>
+      <Link href="/">
+        <button
+          onClick={handleDelete}
+          className="ml-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 cursor-pointer"
+        >
+          削除
+        </button>
+      </Link>
+    </div>
+  );
+};
--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,38 +1,16 @@
-"use client";
-import Link from "next/link";
 import { Container } from "../../components/Container";
 import { Header } from "../../components/Header";
 import { Article } from "@/app/components/Article";
-import { useParams } from "next/navigation";
-import { storageData } from "@/app/utils/storageData";
-import { useCallback } from "react";
+import { EditButtons } from "@/app/components/EditButtons";
 
 export default function Post() {
-  const params = useParams();
-  const posts = storageData("posts");
 
-  const handleDelete = useCallback(() => {
-    const updatedPosts = posts.filter((post) => post.id !== params.id);
-    localStorage.setItem("posts", JSON.stringify(updatedPosts));
-  }, []);
   return (
     <div>
       <Header />
       <Container>
         <Article />
-        <Link href={`/post/${params.id}/edit`}>
-          <button className="mt-8 bg-green-500 text-white px-4 py-2 rounded-md hover:bg-green-600 cursor-pointer">
-            記事を編集
-          </button>
-        </Link>
-        <Link href="/">
-          <button
-            onClick={handleDelete}
-            className="ml-4 px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 cursor-pointer"
-          >
-            削除
-          </button>
-        </Link>
+        <EditButtons />
       </Container>
     </div>
   );
