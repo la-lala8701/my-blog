6554bda8815d0f592d822c1763d4e9bea01a4829
Nakata Saki <usakipyonfln5@gmail.com>
2025-11-27 16:29:07 +0900

    fix: 投稿データの取得方法を修正し、ローディング状態を追加しました

--- app/components/Article/index.tsx
+++ app/components/Article/index.tsx
@@ -1,43 +1,52 @@
 'use client';
-import { PostData } from "../../types";
-import Markdown from "react-markdown";
-import remarkGfm from "remark-gfm";
-import rehypeRaw from "rehype-raw";
-import classes from "./Article.module.css";
-import { usePost } from "@/app/hooks/usePost";
+import { PostData } from '../../types';
+import Markdown from 'react-markdown';
+import remarkGfm from 'remark-gfm';
+import rehypeRaw from 'rehype-raw';
+import classes from './Article.module.css';
+import { usePost } from '@/app/hooks/usePost';
+import { Dispatch, SetStateAction, useEffect, useState } from 'react';
+import { getPostById } from '@/app/utils/supabaseFunctions';
+import { EditButtons } from '../EditButtons';
 
-export const Article = () => {
-  const { params, posts } = usePost();
+export const Article = ({setShowModal}: {setShowModal: Dispatch<SetStateAction<boolean>>}) => {
+  const { params } = usePost();
+  const [post, setPost] = useState<PostData | null>(null);
 
-  const data: PostData | undefined = posts.find(
-    (post) => post.id === params.id
-  );
+  useEffect(() => {
+    const getPost = async () => {
+      const post = await getPostById(params.id);
+      setPost(post);
+    };
+    getPost();
+  }, [params.id]);
 
-  if (!data) {
-    return <div className="font-bold text-2xl">記事が見つかりません。</div>;
-  }
+  if (!post) return <div>ローディング中...</div>;
 
   return (
-    <article>
-      <h1 className="text-5xl leading-normal font-bold">{data.title}</h1>
-      <div className="flex items-start gap-2 mt-12">
-        <div className="w-7 h-7 bg-gray-400 rounded-full"></div>
-        <div>
-          <p className="text-base">{data?.author}</p>
-          <p className="text-sm text-gray-500">{data.created_at}</p>
+    <>
+      <article>
+        <h1 className="text-5xl leading-normal font-bold">{post?.title}</h1>
+        <div className="flex items-start gap-2 mt-12">
+          <div className="w-7 h-7 bg-gray-400 rounded-full"></div>
+          <div>
+            <p className="text-base">{post?.author}</p>
+            <p className="text-sm text-gray-500">{post?.created_at}</p>
+          </div>
         </div>
-      </div>
-      <div className="mt-28 prose prose-lg max-w-none">
-        <section className={classes.markdown}>
-          <Markdown
-            remarkPlugins={[remarkGfm]}
-            skipHtml={false}
-            rehypePlugins={[rehypeRaw]}
-          >
-            {data?.content}
-          </Markdown>
-        </section>
-      </div>
-    </article>
+        <div className="mt-28 prose prose-lg max-w-none">
+          <section className={classes.markdown}>
+            <Markdown
+              remarkPlugins={[remarkGfm]}
+              skipHtml={false}
+              rehypePlugins={[rehypeRaw]}
+            >
+              {post?.content}
+            </Markdown>
+          </section>
+        </div>
+      </article>
+      <EditButtons setShowModal={setShowModal} />
+    </>
   );
 };
