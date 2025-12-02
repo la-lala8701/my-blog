dd37c4a0f5887cb45d97939f81df6e7274a79387
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-02 14:11:08 +0900

    fix: ローカルストレージの記述削除

--- app/utils/storageData.ts
+++
@@ -1,9 +0,0 @@
-import { PostData } from "../types";
-
-export const storageData = (key: "posts"): PostData[] => {
-  if (typeof window !== "undefined") {
-    const storedPosts = localStorage.getItem(key);
-    return storedPosts ? JSON.parse(storedPosts) : [];
-  }
-  return [];
-}
