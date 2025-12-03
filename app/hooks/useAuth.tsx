e54bed57c30cc600ff3d3bfbd744c9d577c01463
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-03 20:30:26 +0900

    add: useAuth追加

---
+++ app/hooks/useAuth.tsx
@@ -0,0 +1,39 @@
+'use client';
+import { supabase } from '@/lib/supabaseFunctions';
+import { useRouter } from 'next/navigation';
+
+export const useAuth = () => {
+  const router = useRouter();
+
+  const signUpUser = async (email: string, password: string) => {
+    try {
+      const { error: signUpError } = await supabase.auth.signUp({
+        email,
+        password,
+      });
+      if (signUpError) {
+        throw signUpError;
+      }
+      await router.push('/login');
+    } catch (error) {
+      alert('サインアップ中にエラーが発生しました。');
+      console.error('Error signing up:', error);
+    }
+  };
+
+  const signInUser = async (email: string, password: string) => {
+    try {
+      const {error: signInError} = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+        if (signInError) { throw signInError; }
+        await router.push('/');
+    } catch (error) {
+        alert('サインイン中にエラーが発生しました。');
+        console.error('Error signing in:', error);
+    }
+  };
+
+  return { signUpUser, signInUser };
+};
