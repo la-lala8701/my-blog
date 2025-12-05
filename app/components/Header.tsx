91c6b19f85c72224ad08c78f24c0ca781e00d373
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-05 16:44:18 +0900

    ✨ ログイン状態で出しわけするロジックの実装

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,25 +1,11 @@
-import { supabase } from '@/lib/supabaseFunctions';
+'use client';
 import Link from 'next/link';
 import { Logout } from './LogOut';
+import { useAuth } from '../hooks/useAuth';
 
 export const Header = () => {
-  supabase.auth.onAuthStateChange((event, session) => {
-    console.log('Auth event:', event);
-    console.log('Session data:', session);
-    if (event === 'INITIAL_SESSION') {
-      // handle initial session
-    } else if (event === 'SIGNED_IN') {
-      // handle sign in event
-    } else if (event === 'SIGNED_OUT') {
-      // handle sign out event
-    } else if (event === 'PASSWORD_RECOVERY') {
-      // handle password recovery event
-    } else if (event === 'TOKEN_REFRESHED') {
-      // handle token refreshed event
-    } else if (event === 'USER_UPDATED') {
-      // handle user updated event
-    }
-  });
+  const { session } = useAuth();
+
   return (
     <header className="py-6 px-14 border-b border-gray-300">
       <div className="flex items-center justify-between">
@@ -28,33 +14,40 @@ export const Header = () => {
         </Link>
         <nav>
           <ul className="flex items-center">
-            <li>
-              <Link
-                className="px-4 py-3 bg-blue-300 rounded-md hover:bg-blue-400"
-                href="/login"
-              >
-                ログイン
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="ml-4 px-4 py-3 bg-blue-300 rounded-md hover:bg-blue-400"
-                href="/auth/sign-up"
-              >
-                サインアップ
-              </Link>
-            </li>
-            <li>
-              <Link
-                className="ml-4 px-4 py-3 bg-green-300 rounded-md hover:bg-green-400"
-                href="/create"
-              >
-                記事作成
-              </Link>
-            </li>
-            <li className="ml-4">
-              <Logout />
-            </li>
+            {session ? (
+              <>
+                <li>
+                  <Link
+                    className="ml-4 px-4 py-3 bg-green-300 rounded-md hover:bg-green-400"
+                    href="/create"
+                  >
+                    記事作成
+                  </Link>
+                </li>
+                <li className="ml-4">
+                  <Logout />
+                </li>
+              </>
+            ) :(
+              <>
+                <li>
+                  <Link
+                    className="px-4 py-3 bg-blue-300 rounded-md hover:bg-blue-400"
+                    href="/login"
+                  >
+                    ログイン
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className="ml-4 px-4 py-3 bg-yellow-300 rounded-md hover:bg-yellow-400"
+                    href="/auth/sign-up"
+                  >
+                    サインアップ
+                  </Link>
+                </li>
+              </>
+            )}
           </ul>
         </nav>
       </div>
--- app/components/PostWrapper.tsx
+++ app/components/PostWrapper.tsx
@@ -3,14 +3,16 @@
 import { useState } from 'react';
 import { DeleteModal } from './DeleteModal';
 import { EditButtons } from './EditButtons';
+import { useAuth } from '../hooks/useAuth';
 
 export const PostWrapper = ({ children }: { children: React.ReactNode }) => {
   const [showModal, setShowModal] = useState(false);
+  const { session } = useAuth();
   return (
     <>
       {children}
       <DeleteModal showModal={showModal} setShowModal={setShowModal} />
-      <EditButtons setShowModal={setShowModal} />
+      {session ? <EditButtons setShowModal={setShowModal} /> : null}
     </>
   );
 };
