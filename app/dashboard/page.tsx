70b5c651e82bd3e62f20e0ecbc0317904fb1ff44
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-09 10:25:10 +0900

    ✨プロフィールコンポーネントに変更

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { Profile } from '../components/Profile';
 
 export default function Dashboard() {
   return (
@@ -7,17 +8,7 @@ export default function Dashboard() {
       <p className="text-center mt-4 text-gray-600">
         ようこそ、ダッシュボードへ！ここからブログの管理や設定が行えます。
       </p>
-      <div className="mt-10 flex flex-col items-center gap-4">
-        <div className="w-32 h-32 bg-gray-300 rounded-full text-gray-100 flex items-center justify-center">
-          画像
-        </div>
-        <div>
-          <p className="text-xl font-semibold text-center">
-            ユーザー名
-          </p>
-          <p className="mt-2 text-sm max-w-sm">ここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入ります</p>
-        </div>
-      </div>
+      <Profile />
       <ul className="flex flex-wrap gap-6 justify-center mt-16">
         <li>
           <Link
