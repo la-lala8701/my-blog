82620b5b117c6dec638461687c257b18c0466329
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-09 10:24:44 +0900

    ✨ プロフィールコンポーネント作成

---
+++ app/components/Profile.tsx
@@ -0,0 +1,15 @@
+export const Profile = () => {
+  return (
+    <div className="mt-10 flex flex-col items-center gap-4">
+      <div className="w-32 h-32 bg-gray-300 rounded-full text-gray-100 flex items-center justify-center">
+        画像
+      </div>
+      <div>
+        <p className="text-xl font-semibold text-center">ユーザー名</p>
+        <p className="mt-2 text-sm max-w-sm">
+          ここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入ります
+        </p>
+      </div>
+    </div>
+  );
+};
