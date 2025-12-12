038cdc409ae7c77084e5e8b82548f64de6532a5b
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-12 11:16:07 +0900

    ♻️useForm を使った実装に修正し、ログインしているユーザー名が記事に表示されるように修正

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -1,146 +1,75 @@
 'use client';
-import { useCallback, useReducer } from 'react';
 import { v4 as uuidv4 } from 'uuid';
 import Link from 'next/link';
 import { PostData } from '@/app/types';
-import { addPost } from '@/lib/supabaseFunctions';
+import { addPost, supabase } from '@/lib/supabaseFunctions';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
 
-type ChangeValue = {
+type Inputs = {
   title: string;
   content: string;
-  author: string;
-};
-
-type Action =
-  | { type: 'change_author'; nextValue: string }
-  | { type: 'change_title'; nextValue: string }
-  | { type: 'change_content'; nextValue: string }
-  | { type: 'finished' };
-
-const init = (): ChangeValue => {
-  return {
-    title: '',
-    content: '',
-    author: '',
-  };
-};
-
-const reducer = (state: ChangeValue, action: Action) => {
-  switch (action.type) {
-    case 'change_author':
-      return {
-        ...state,
-        author: action.nextValue,
-      };
-    case 'change_title':
-      return {
-        ...state,
-        title: action.nextValue,
-      };
-    case 'change_content':
-      return {
-        ...state,
-        content: action.nextValue,
-      };
-    case 'finished':
-      return {
-        ...state,
-        title: '',
-        content: '',
-        author: '',
-      };
-  }
 };
 
 export default function CreatePage() {
-  const [state, dispatch] = useReducer(reducer, null, init);
+  const { handleSubmit, register, reset } = useForm<Inputs>();
+  const router = useRouter();
   const id = uuidv4();
 
-  const handleChangeAuthor = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      dispatch({ type: 'change_author', nextValue: e.target.value });
-    },
-    [],
-  );
-
-  const handleChangeTitle = useCallback(
-    (e: React.ChangeEvent<HTMLInputElement>) => {
-      dispatch({ type: 'change_title', nextValue: e.target.value });
-    },
-    [],
-  );
-
-  const handleChangeContent = useCallback(
-    (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-      dispatch({ type: 'change_content', nextValue: e.target.value });
-    },
-    [],
-  );
-
-  const handleSubmit = useCallback(
-    async (e: React.FormEvent<HTMLFormElement>) => {
-      e.preventDefault();
-
-      // 全てのフィールドに入力されているか確認
-      if (!state.title || !state.content || !state.author) {
-        alert('全てのフィールドを入力してください。');
+  const onSubmit: SubmitHandler<Inputs> = async (data) => {
+    try {
+      // 現在のユーザー情報を取得
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      if (!user) {
+        alert('ログインしていません');
         return;
       }
 
-      // 新規データ
+      const displayName = user.user_metadata.display_name
+
+      // 新規記事データ
       const newPost: PostData = {
         id,
-        title: state.title,
-        content: state.content,
-        author: state.author,
+        user_id: user.id,
+        title: data.title,
+        content: data.content,
+        author: displayName,
         created_at: new Date().toISOString(),
       };
 
+      // 記事をpostsテーブルに挿入する
       await addPost(newPost);
-
-      // 新規データを追加し、フォームをリセット
-      dispatch({ type: 'finished' });
-      alert('記事が作成されました！');
-      location.href = `/post/${id}`;
-    },
-    [state.title, state.content, state.author, id],
-  );
+      // フォームをリセット
+      reset();
+      // 投稿した記事ページへリダイレクト
+      router.push(`/post/${id}`)
+    } catch (error) {
+      console.error('予期せぬエラー', error);
+    }
+  };
 
   return (
     <>
       <h1 className="text-2xl font-bold mb-4">記事作成</h1>
-      <form
-        className="space-y-4"
-        onSubmit={handleSubmit}
-        action={`/post/${id}`}
-        suppressHydrationWarning
-      >
-        <div>
-          <label className="block mb-1">著者名</label>
-          <input
-            type="text"
-            value={state.author}
-            onChange={handleChangeAuthor}
-            className="border border-gray-300 rounded-md p-2 w-full"
-          />
-        </div>
+      <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        {/* タイトル */}
         <div>
           <label className="block mb-1">タイトル</label>
           <input
             className="border border-gray-300 rounded-md p-2 w-full"
-            type="text"
-            value={state.title}
-            onChange={handleChangeTitle}
+            {...register('title', { required: true })}
           />
         </div>
+        {/* 内容 */}
         <div>
           <label className="block mb-1">内容</label>
           <textarea
             className="border border-gray-300 rounded-md p-2 w-full"
             rows={10}
-            value={state.content}
             placeholder="マークダウンで内容を記載してください（GitHub Flavored Markdownをサポートしています）"
-            onChange={handleChangeContent}
+            {...register('content', { required: true })}
           ></textarea>
         </div>
         <button
