f50b872d43a19c8660e0956452e12a0734cfbf19
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-15 19:37:38 +0900

    ✨ データベースからの取得に変更

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -1,11 +1,16 @@
+'use client';
+import { useAuth } from '../hooks/useAuth';
+
 export const Profile = () => {
+  const { context } = useAuth();
+
   return (
     <div className="mt-10 flex flex-col items-center gap-4">
       <div className="w-32 h-32 bg-gray-300 rounded-full text-gray-100 flex items-center justify-center">
         画像
       </div>
       <div>
-        <p className="text-xl font-semibold text-center">ユーザー名</p>
+        <p className="text-xl font-semibold text-center">{context.session?.user.user_metadata.display_name}</p>
         <p className="mt-2 text-sm max-w-sm">
           ここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入りますここに自己紹介文が入ります
         </p>
