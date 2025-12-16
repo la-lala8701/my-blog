10f4642c1e9e8ea208c5d1b21738f5fa22d92235
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-16 23:55:05 +0900

    🚧記事を書いた人のプロフィールを投稿記事に反映させようとしています

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -2,6 +2,7 @@ import { Article } from '@/app/components/Article';
 import { PostWrapper } from './PostWrapper';
 import { PostData } from '../types';
 import { getPostById } from '@/lib/supabaseFunctions';
+import { Profile } from './Profile';
 
 export const Post = async ({ pageId }: { pageId: string }) => {
   const getPost: PostData = await getPostById(pageId);
@@ -9,6 +10,7 @@ export const Post = async ({ pageId }: { pageId: string }) => {
   return (
     <PostWrapper>
       <Article post={getPost} />
+      <Profile author={getPost.author} />
     </PostWrapper>
   );
 };
