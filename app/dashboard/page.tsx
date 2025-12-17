9a8a39b44d29624e483fd4a2596051c854da117f
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-17 17:54:54 +0900

    🔥プロフィールコンポーネントの表示はやめました

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,4 @@
 import Link from 'next/link';
-import { Profile } from '../components/Profile';
 
 export default function Dashboard() {
   return (
@@ -8,7 +7,6 @@ export default function Dashboard() {
       <p className="text-center mt-4 text-gray-600">
         ようこそ、ダッシュボードへ！ここからブログの管理や設定が行えます。
       </p>
-      <Profile />
       <ul className="flex flex-wrap gap-6 justify-center mt-16">
         <li>
           <Link
