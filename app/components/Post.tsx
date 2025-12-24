3afa443a3f10e2f852b9c2528b95f3fb97c429df
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-24 19:10:09 +0900

    🔥 本番環境の記事一覧に修正

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -1,5 +1,4 @@
 import { Article } from '@/app/components/Article';
-import { PostWrapper } from './PostWrapper';
 import { PostData } from '../types';
 import { getPostById } from '@/lib/supabaseFunctions';
 import { Profile } from './Profile';
@@ -9,12 +8,10 @@ export const Post = async ({ pageId }: { pageId: string }) => {
 
   return (
     <div className="flex gap-10 mx-auto max-w-5xl mt-12">
-      <div className='max-w-3xl flex-1'>
-        <PostWrapper>
-          <Article post={getPost} />
-        </PostWrapper>
+      <div className="max-w-3xl flex-1">
+        <Article post={getPost} />
       </div>
-      <div className='w-56 sticky top-12 h-fit'>
+      <div className="w-56 sticky top-12 h-fit">
         <Profile userId={getPost.user_id} />
       </div>
     </div>
