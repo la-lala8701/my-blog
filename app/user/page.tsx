a1852573fd7692ee8f42fcb162079faf7fd77dda
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-24 19:06:50 +0900

    ⚗️ url部分がうまくいっていない

---
+++ app/user/page.tsx
@@ -0,0 +1,58 @@
+import Link from 'next/link';
+import { Profile } from '../components/Profile';
+import { createClient } from '@/lib/supabase/server';
+import { PostData } from '../types';
+import { getUserPosts } from '@/lib/supabaseFunctions';
+import { notFound } from 'next/navigation';
+import { Posts } from '../components/Posts';
+
+export default async function Dashboard() {
+  // 現在のユーザー情報を取得
+  const {
+    data: { user },
+    error,
+  } = await (await createClient()).auth.getUser();
+  if (error || !user) {
+    console.error('認証セッションが不正です');
+    return;
+  }
+
+  // ユーザーが書いた記事を取得する
+  const posts: PostData[] | null = await getUserPosts(user.id);
+
+  if (!posts || posts.length === 0) {
+    notFound();
+  }
+
+  return (
+    <div className="max-w-5xl mx-auto pt-12 pb-16">
+      <h1 className="text-4xl font-bold mb-8 text-center">マイページ</h1>
+      <div className='flex gap-10'>
+        <div className='w-44'>
+          <Profile userId={user.id} />
+          <ul className='mt-9'>
+            {/* <li>
+              <Link
+                href="/user/posts"
+                className="border p-4 rounded-md hover:text-blue-500"
+              >
+                記事管理 →
+              </Link>
+            </li> */}
+            <li>
+              <Link
+                href="/user/settings"
+                className="bg-gray-700 text-white px-4 py-2.5 rounded-md hover:bg-gray-500 block text-center"
+              >
+                設定 →
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className='flex-auto'>
+          <Posts postsData={posts} />
+        </div>
+      </div>
+    </div>
+  );
+}
