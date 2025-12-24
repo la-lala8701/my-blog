df053c36531cdfc1947c434c51546e6c699241ec
Nakata Saki <usakipyonfln5@gmail.com>
2025-12-24 19:12:37 +0900

    🎨 記事ページ修正

--- app/post/[id]/page.tsx
+++ app/post/[id]/page.tsx
@@ -1,7 +1,25 @@
-import { Post as PostComponent } from '@/app/components/Post';
+import { Article } from '@/app/components/Article';
+import { PostEdit } from '@/app/components/PostEdit';
+import { Profile } from '@/app/components/Profile';
+import { PostData } from '@/app/types';
+import { getPostById } from '@/lib/supabaseFunctions';
 
-export default async function Post({ params }: { params: { id: string } }) {
-  const { id } = await params;
+export default async function Post({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const pageId: string = (await params).id;
+  const getPost: PostData = await getPostById(pageId);
 
-  return <PostComponent pageId={id} />;
+  return (
+    <div className="flex gap-10 mx-auto max-w-5xl mt-12 mb-16">
+      <div className="max-w-3xl flex-1">
+        <Article post={getPost} />
+      </div>
+      <div className="w-56 sticky top-12 h-fit">
+        <Profile userId={getPost.user_id} />
+      </div>
+    </div>
+  );
 }
