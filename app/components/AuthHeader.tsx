3b364e9517451eca3b34b09446621ada1d946ebc
Nakata Saki <usakipyonfln5@gmail.com>
2026-01-13 15:52:55 +0900

    ♻️ドロワーメニューをコンポーネント化し、リンクをクリックしたときにメニューが閉じるようにしました

--- app/components/AuthHeader.tsx
+++ app/components/AuthHeader.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
-import { Logout } from './LogOut';
 import Avatar from 'boring-avatars';
 import { useState } from 'react';
+import { Drawer } from './Drawer';
 
 export const AuthHeader = ({
   displayName,
@@ -33,62 +33,12 @@ export const AuthHeader = ({
         </ul>
       </nav>
       {/* メニュー */}
-      {showMenu ? (
-        <div className='fixed w-full h-full top-0 left-0 z-10' onClick={(e) => e.target === e.currentTarget && setShowMenu(false)}>
-          <div className="w-64 absolute top-18 right-6 bg-white border border-gray-200 rounded-md shadow-lg p-4 z-10">
-            <div className="flex flex-col gap-2 items-center mx-2">
-              <div className="w-16 h-16 rounded-full shrink-0">
-                <Avatar name={displayName} size={64} variant="beam" />
-              </div>
-              <div className="text-lg font-semibold truncate w-full text-center">{displayName}</div>
-              <div className="text-gray-600 truncate w-full text-center">{userEmail}</div>
-            </div>
-            <dl className="mt-8">
-              <div>
-                <dt className="text-gray-500 text-sm p-1">管理</dt>
-                <dd>
-                  <Link href="/user" className="p-4 block hover:bg-gray-100">
-                    マイページ
-                  </Link>
-                </dd>
-              </div>
-              <div className="mt-3">
-                <dt className="text-gray-500 text-sm p-1">設定</dt>
-                <dd>
-                  <ul>
-                    <li>
-                      <Link
-                        href="/user/settings/profile"
-                        className="p-4 block hover:bg-gray-100"
-                      >
-                        プロフィール
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        href="/user/settings/password"
-                        className="p-4 block hover:bg-gray-100"
-                      >
-                        パスワード
-                      </Link>
-                    </li>
-                    <li>
-                      <Link
-                        href="/user/settings/email"
-                        className="p-4 block hover:bg-gray-100"
-                      >
-                        メールアドレス
-                      </Link>
-                    </li>
-                  </ul>
-                </dd>
-              </div>
-            </dl>
-            <hr className="my-4 border-gray-300" />
-            <Logout />
-          </div>
-        </div>
-      ) : null}
+      <Drawer
+        isOpenMenu={showMenu}
+        onClose={() => setShowMenu(false)}
+        displayName={displayName}
+        userEmail={userEmail}
+      />
     </div>
   );
 };
---
+++ app/components/Drawer.tsx
@@ -0,0 +1,77 @@
+import Avatar from 'boring-avatars';
+import Link from 'next/link';
+import { Logout } from './LogOut';
+
+type DrawerProps = {
+  isOpenMenu: boolean;
+  onClose: () => void;
+  displayName: string | undefined;
+  userEmail: string | undefined;
+};
+
+const DrawerMenu = [
+  {
+    title: '管理',
+    items: [{ text: 'マイページ', href: '/user' }],
+  },
+  {
+    title: '設定',
+    items: [
+      { text: 'プロフィール', href: '/user/settings/profile' },
+      { text: 'パスワード', href: '/user/settings/password' },
+      { text: 'メールアドレス', href: '/user/settings/email' },
+    ],
+  },
+];
+
+export const Drawer = (props: DrawerProps) => {
+  if (!props.isOpenMenu) return null;
+
+  return (
+    <div
+      className="fixed w-full h-full top-0 left-0 z-10"
+      onClick={(e) => e.target === e.currentTarget && props.onClose()}
+    >
+      {/* ユーザー情報 */}
+      <div className="w-64 absolute top-18 right-6 bg-white border border-gray-200 rounded-md shadow-lg p-4 z-10">
+        <div className="flex flex-col gap-2 items-center mx-2">
+          <div className="w-16 h-16 rounded-full shrink-0">
+            <Avatar name={props.displayName} size={64} variant="beam" />
+          </div>
+          <div className="text-lg font-semibold truncate w-full text-center">
+            {props.displayName}
+          </div>
+          <div className="text-gray-600 truncate w-full text-center">
+            {props.userEmail}
+          </div>
+        </div>
+        {/* メニュー */}
+        <dl className="mt-8 space-y-3">
+          {DrawerMenu.map((menu) => (
+            <div key={menu.title}>
+              <dt className="text-gray-500 text-sm p-1">{menu.title}</dt>
+              <dd>
+                <ul>
+                  {menu.items.map((item) => (
+                    <li key={item.text}>
+                      <Link
+                        href={item.href}
+                        className="p-4 block hover:bg-gray-100"
+                        onClick={() => props.onClose()}
+                      >
+                        {item.text}
+                      </Link>
+                    </li>
+                  ))}
+                </ul>
+              </dd>
+            </div>
+          ))}
+        </dl>
+        <hr className="my-4 border-gray-300" />
+        {/* ログアウト */}
+        <Logout />
+      </div>
+    </div>
+  );
+};
