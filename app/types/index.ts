c66b7b2e90ce1aa5454060655fa5e2bcb78eb8a2
Nakata Saki <usakipyonfln5@gmail.com>
2026-02-04 17:36:55 +0900

    🏷️型ModalContentを追加

--- app/types/index.ts
+++ app/types/index.ts
@@ -24,3 +24,11 @@ export type AuthInputType = {
   email: string;
   password: string;
 };
+
+export type ModalContent = {
+  title: string;
+  message: string;
+  bgColor: string;
+  hoverColor: string;
+  handleAction: () => Promise<void>;
+};
