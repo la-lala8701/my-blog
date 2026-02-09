8e36e304569ddcf4f1fe797189ddd8bc00dd779f
Nakata Saki <usakipyonfln5@gmail.com>
2026-02-09 15:37:50 +0900

    🎨検索の実装をformタグにしました

--- app/components/Search.tsx
+++ app/components/Search.tsx
@@ -1,15 +1,22 @@
 'use client';
 
 import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+type SearchInputType = {
+  term: string;
+};
 
 export const Search = () => {
+  const { register, handleSubmit } = useForm<SearchInputType>();
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
-  const handleSearch = (term: string) => {
+
+  const onSubmit: SubmitHandler<SearchInputType> = (data) => {
     const params = new URLSearchParams(searchParams);
-    if (term) {
-      params.set('query', term);
+    if (data.term) {
+      params.set('query', data.term);
     } else {
       params.delete('query');
     }
@@ -17,12 +24,15 @@ export const Search = () => {
   };
   return (
     <div className="w-full max-w-md mx-auto">
-      <input
-        type="text"
-        placeholder="Search..."
-        className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-        onChange={(e) => handleSearch(e.target.value)}
-      />
+      <form action="" method='post' onSubmit={handleSubmit(onSubmit)}>
+        <input
+          type="text"
+          placeholder="Search..."
+          className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+          {...register('term')}
+          defaultValue={searchParams.get('query')?.toString()}
+        />
+      </form>
     </div>
   );
 };
