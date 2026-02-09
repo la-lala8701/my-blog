3927a470a43d96b12675c272871977a3bd470b5e
Nakata Saki <usakipyonfln5@gmail.com>
2026-02-09 17:37:29 +0900

    🚧 Homeコンポーネントに検索パラメータを追加し、フィルタリング機能を実装

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,25 @@ import { Posts } from './components/Posts';
 import { notFound } from 'next/navigation';
 import { createClient } from '@/lib/supabase/server';
 
-export default async function Home() {
+export default async function Home({searchParams}: {searchParams?: { query?: string; page?: string}}) {
   const supabase = await createClient();
-  const posts = await getPublicPosts(supabase);
+  // const posts = await getPublicPosts(supabase);
+
+  const fetchfilteredPosts = async () => {
+    const query: string = (await searchParams)?.query || '';
+    if (!query) {
+      return await getPublicPosts(supabase);
+    } else {
+      const { data, error } = await supabase
+        .from('posts')
+        .select()
+        .or(`title.ilike.%${query}%,content.ilike.%${query}%`)
+        .eq('is_published', true)
+      if (error) throw error;
+      return data;
+    }
+  }
+  const posts = await fetchfilteredPosts();
 
   if (!posts || posts.length === 0) {
     notFound();
