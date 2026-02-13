edad7d7002a03a8cc55b69a0b8a2b7348b9e0907
Nakata Saki <usakipyonfln5@gmail.com>
2026-02-13 11:31:06 +0900

    ✨ AuthLayoutとMainLayoutを追加し、RootLayoutからHeaderを削除しました

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { Header } from './components/Header';
 import { AuthProvider } from './context/AuthContext';
 
 const geistSans = Geist({
@@ -29,10 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <AuthProvider>
-          <Header />
-          {children}
-        </AuthProvider>
+        <AuthProvider>{children}</AuthProvider>
       </body>
     </html>
   );
