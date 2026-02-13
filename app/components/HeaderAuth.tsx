a7c3e4d4f9902ceed000bd7bdb7a7a79d225c19a
Nakata Saki <usakipyonfln5@gmail.com>
2026-02-13 14:42:35 +0900

    ✨ログイン・サインアップ専用のヘッダー作成

---
+++ app/components/HeaderAuth.tsx
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export const HeaderAuth = () => {
+
+  return (
+    <header className="py-6">
+      <div className="max-w-7xl mx-auto px-4 flex items-center justify-between">
+        <Link className="p-1.5 text-2xl font-bold" href="/">
+          MyBlog
+        </Link>
+      </div>
+    </header>
+  );
+};
