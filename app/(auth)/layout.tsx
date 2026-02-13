ca94421113c4a8310c26117c6b12a5d19a92aea1
Nakata Saki <usakipyonfln5@gmail.com>
2026-02-13 14:43:16 +0900

    ✨ヘッダーの追加

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,7 +1,14 @@
+import { HeaderAuth } from '../components/HeaderAuth';
+
 export default function AuthLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <>{children}</>;
+  return (
+    <div className="px-4">
+      <HeaderAuth />
+      {children}
+    </div>
+  );
 }
