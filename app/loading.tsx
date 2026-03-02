937dd2c53a4ef86d6b3b1412026b6a2332e781bd
Nakata Saki <usakipyonfln5@gmail.com>
2026-03-02 13:41:03 +0900

    💄ロディングのスタイル調整

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,7 @@
 export default function Loading() {
-    return <p>Loading</p>
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p className="text-center text-lg">Now Loading...</p>
+    </div>
+  );
 }
