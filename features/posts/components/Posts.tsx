f4696851bc500538b4304ee2efc239663aef1255
Nakata Saki <usakipyonfln5@gmail.com>
2026-03-04 10:51:00 +0900

    🩹 directory case sensitivity issue (Posts to posts)

---
+++ features/posts/components/Posts.tsx
@@ -0,0 +1,18 @@
+import { PostsItem } from '@/features/posts/components/PostsItem';
+import { PostData } from '@/app/types';
+
+export const Posts = async ({
+  postsData,
+  manage,
+}: {
+  postsData: PostData[];
+  manage?: boolean;
+}) => {
+  return (
+    <div className="grid grid-cols-1 gap-4">
+      {postsData.map((post: PostData) => (
+        <PostsItem key={post.id} {...post} manage={manage} />
+      ))}
+    </div>
+  );
+};
