7ff89371eadae464d2a0b56f2edf764e9f9aa0cd
Nakata Saki <usakipyonfln5@gmail.com>
2026-03-18 11:47:44 +0900

    ♻️ ローディング画面改善

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,7 +1,9 @@
 export default function Loading() {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <p className="text-center text-lg">Now Loading...</p>
+    <div className="flex justify-center my-20 space-x-4" aria-label="読み込み中">
+      <div className="animate-ping h-2 w-2 bg-gray-600 rounded-full"></div>
+      <div className="animate-ping h-2 w-2 bg-gray-600 rounded-full"></div>
+      <div className="animate-ping h-2 w-2 bg-gray-600 rounded-full"></div>
     </div>
   );
 }
