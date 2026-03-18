5f5ad2f29f4bda1d7e6a1918fcf62e6c4e238412
Nakata Saki <usakipyonfln5@gmail.com>
2026-03-18 15:23:34 +0900

    💄 ヘッダー部分のスタイル調整

--- app/components/layouts/Header/Header.tsx
+++ app/components/layouts/Header/Header.tsx
@@ -6,14 +6,14 @@ export const Header = () => {
   return (
     <header className="py-6 border-b border-gray-300">
       {/* SP時検索窓だけ下げたい */}
-      <div className="max-w-7xl mx-auto px-4 grid grid-cols-2 items-center gap-4 md:grid-cols-1 md:flex md:justify-between">
-        <Link className="p-1.5 text-2xl font-bold col-start-1" href="/">
+      <div className="w-full max-w-7xl mx-auto px-4 grid grid-cols-2 items-center gap-4 md:grid-cols-1 md:flex md:justify-between">
+        <Link className="p-1.5 text-2xl font-bold col-start-1 inline-block" href="/">
           MyBlog
         </Link>
         <div className="order-3 col-span-2 md:order-2 md:w-2/5">
           <Search />
         </div>
-        <div className="order-2 col-start-2 flex justify-end -ml-10 md:order-3">
+        <div className="order-2 col-start-2 flex justify-end -ml-10 md:order-3 md:ml-0">
           <HeaderNavi />
         </div>
       </div>
